120a24bc157b78b0e37bfbfad76df82a2868adfd
Daastan <daastan@MacBook-Pro-3.local>
2025-01-26 13:58:10 -0500

    Fix Gunicorn deployment by aliasing app as application

--- app.py
+++ app.py
@@ -84,3 +84,6 @@ if __name__ == '__main__':
     import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
+
+# Alias Flask app for Gunicorn
+application = app
