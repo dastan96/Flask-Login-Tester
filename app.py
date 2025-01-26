f2a9fbb6047fd9b6594eaf2b786ad660a14f5377
Daastan <daastan@MacBook-Pro-3.local>
2025-01-26 13:44:49 -0500

    Update requirements and app.py for deployment

--- app.py
+++ app.py
@@ -86,3 +86,9 @@ def welcome():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+    if __name__ == '__main__':
+    # Use the port provided by Render during deployment
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
